678dd9deeb52f1d31ae7dfcf21dc2ada4c2db5c6
James Tiplady <biggleszx@biggleszx.com>
2022-09-01 17:07:36 +0100

    Adding test endpoint and netlify.toml

---
+++ pages/_foo/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from "../../styles/Home.module.css";
+
+const Foo: NextPage = () => {
+  return <div className={styles.container}>Foo</div>;
+};
+
+export default Foo;
